ac0406fad0ce9f82c9bffda0642015beb06c44ad
Soren Macbeth <soren@dopeness.org>
2013-05-24 09:31:33 -0700

    update

--- src/hll_test/core.clj
+++ src/hll_test/core.clj
@@ -2,6 +2,7 @@
   (:use cascalog.api
         al-jabr.core))
 
+;; the parameter here is the accepted error percentage
 (def HLL* (mk-hyper-log-log 1.0))
 
 (defn hll-combine [l r] (.plus HLL* l r))
@@ -11,15 +12,20 @@
 (defmapop to-hll [x] (->hll HLL* #(.getBytes % "UTF-8") x))
 (defmapop from-hll [x] (.estimatedSize x))
 
-(defn map-plus-map [to-fn from-fn] (<- [?x :> ?y] (to-fn ?x :> ?item) (hll-plus ?item :> ?reduced) (from-fn ?reduced :> ?y)))
+(defn map-plus-map [to-fn from-fn]
+  (<- [?x :> ?y] (to-fn ?x :> ?item)
+      (hll-plus ?item :> ?reduced)
+      (from-fn ?reduced :> ?y)))
 
 (def hll-mpm (map-plus-map to-hll from-hll))
 
-(defn test []
-    (let [src [["foo"] ["bar"] ["baz"] ["pickle"] ["zombie"] ["winchester"]]]
-      (use 'cascalog.playground)
-      (bootstrap-emacs)
-      (?<- (stdout)
-           [?distinct-size]
-           (src ?string)
-           (hll-mpm ?string :> ?distinct-size))))
+(defn test
+  "6.0"
+  []
+  (let [src [["foo"] ["bar"] ["baz"] ["pickle"] ["zombie"] ["winchester"]]]
+    (use 'cascalog.playground)
+    (bootstrap-emacs)
+    (?<- (stdout)
+         [?distinct-size]
+         (src ?string)
+         (hll-mpm ?string :> ?distinct-size))))
